460faab5ab65876c3b5b5f959faf8430a6881707
RJ Dellecese <rjdellecese@gmail.com>
2015-11-27 23:30:50 +0000

    wrote test for unique_tag, implemented in view

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "unique_tag" do
+    quote =  Quote.create(:author => 'RJ Dellecese', :saying => 'Man is born free, but everywhere he is in chains.')
+    expected = 'RD#' + quote.id.to_s
+    actual = quote.unique_tag
+    assert_equal expected, actual
   end
 end
