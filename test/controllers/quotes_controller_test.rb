5052fd15066fd1174c196fd222cfd0daa2dcca53
RJ Dellecese <rjdellecese@gmail.com>
2015-11-28 22:28:38 +0000

    added passing controller test for show page for individual quote ids

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class QuotesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "quote show page" do
+    quote =  Quote.create(:author => 'RJ Dellecese', :saying => 'Man is born free, but everywhere he is in chains.')
+    get :show, :id => quote.id
+    assert_response :success
+  end
+
+  test "quote show page, not found" do
+    get :show, :id => 'fake'
+    assert_response :not_found
+  end
 end
